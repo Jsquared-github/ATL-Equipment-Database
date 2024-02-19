e1c0c6b4358bd39e0c52949e014c92870f3476c6
Joshua Johnston <2021johnston.joshua@gmail.com>
2024-02-19 13:05:02 -0500

    Created database with init_db.sql

---
+++ Backend/database/sql/init_db.sql
@@ -0,0 +1,50 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE user_auth_info(
+    userID INTEGER PRIMARY KEY,
+    pass TEXT,
+    email TEXT,
+    username TEXT,
+    category TEXT
+);
+
+CREATE TABLE team_info(
+    teamID INTEGER PRIMARY KEY,
+    teamName TEXT
+);
+
+CREATE TABLE equipment_info(
+    equipmentID INTEGER PRIMARY KEY,
+    equipName TEXT,
+    unitPrice FLOAT,
+    initQuantity INTEGER,
+    currQuantity INTEGER
+);
+
+CREATE TABLE player_info(
+    pID INTEGER,
+    tID INTEGER,
+    PRIMARY KEY(pID,tID),
+    FOREIGN KEY(pID) REFERENCES user_auth_info(userID),
+    FOREIGN KEY(tID) REFERENCES team_info(teamID)
+)WITHOUT ROWID;
+
+CREATE TABLE coach_info(
+    coachID INTEGER,
+    tID INTEGER,
+    PRIMARY KEY(coachID,tID),
+    FOREIGN KEY(coachID) REFERENCES user_auth_info(userID),
+    FOREIGN KEY(tID) REFERENCES team_info(teamID)
+)WITHOUT ROWID;
+
+CREATE TABLE activity_log(
+    cID INTEGER,
+    tID INTEGER,
+    coDate TEXT,
+    eID INTEGER,
+    equipDiff INTEGER,
+    PRIMARY KEY(cID,tID, coDate),
+    FOREIGN KEY(cID) REFERENCES coach_info(coachID),
+    FOREIGN KEY(tID) REFERENCES team_info(teamID),
+    FOREIGN KEY(eID) REFERENCES equipment_info(equpmentID)
+)WITHOUT ROWID;
